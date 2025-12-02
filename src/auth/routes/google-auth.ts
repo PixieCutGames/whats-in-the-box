c9e9bba01332b2e5feca0bb470354b3220519d4b
Dina Abuzied <dina.abuzied@gmail.com>
2025-12-02 12:35:32 +0200

    Update google-auth.ts

--- src/auth/routes/google-auth.ts
+++ src/auth/routes/google-auth.ts
@@ -6,7 +6,7 @@ const googleAuthRoute: FastifyPluginAsync = async (fastify, opts) => {
       "https://accounts.google.com/o/oauth2/v2/auth?" +
       new URLSearchParams({
         client_id: process.env.GOOGLE_CLIENT_ID!,
-        redirect_uri: process.env.GOOGLE_REDIRECT_URI!,
+        redirect_uri: `${request.protocol}://${request.host}/auth/google/callback`,
         response_type: "code",
         scope: "openid email profile",
         access_type: "offline",
